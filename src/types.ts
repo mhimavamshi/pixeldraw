facf4812944a8a76fae1da0940c19d6050f12dc9
mhimavamshi <himavamshi1212@gmail.com>
2026-02-09 23:23:38 +0530

    add GridInput (circular, but for now)

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,15 @@
+import { Grid } from "./classes";
+
 type Point = {
     x: number,
     y: number,
 }
 
-export { Point };
+// also i know its circular imports, but i dont know why i want to seperate types and classes
+type GridInput = {
+    grid: Grid, 
+    metadata: Record<string, any> // weak for now
+}
+
+
+export { Point, GridInput };
