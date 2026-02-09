e3441d2fc747d4aa7190c60c8f590293013c1245
mhimavamshi <himavamshi1212@gmail.com>
2026-02-09 23:58:31 +0530

    a different design?

--- src/main.ts
+++ src/main.ts
@@ -7,3 +7,4 @@ editor.start();
 
 
 
+// to something like tools.setup(editor) and some interface between them like tools and editors are separate, and maybe tools listen to events emitted by editor? with event data
