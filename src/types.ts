b44495b80b11e844baa74f6e8fd957da7d60fce3
mhimavamshi <himavamshi1212@gmail.com>
2026-02-10 20:41:57 +0530

    add (Grid|Tool)Type interface, restrict GridInput to take in event for now, ToolRegistry type

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-import { Grid } from "./classes";
+import { Grid, Tool } from "./classes";
 
 type Point = {
     x: number,
@@ -8,8 +8,24 @@ type Point = {
 // also i know its circular imports, but i dont know why i want to seperate types and classes
 type GridInput = {
     grid: Grid, 
-    metadata: Record<string, any> // weak for now
+    event: Event 
 }
 
 
-export { Point, GridInput };
+interface ToolType {
+    description: string;
+    name: string;
+    register(registry: Record<string, ToolType>): void;
+    effect(grid: Grid): void;
+}
+
+interface GridToolType extends ToolType {
+    pointerDown?(gridInput: GridInput): void,
+    pointerUp?(gridInput: GridInput): void,
+    pointerCancel?(gridInput: GridInput): void,
+    pointerMove(gridInput: GridInput): void,
+}
+
+type ToolRegistry = Record<string, GridToolType | ToolType>;
+
+export { Point, GridInput, ToolRegistry, ToolType, GridToolType };
